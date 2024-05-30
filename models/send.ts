32b1efb77336db68ce0fdf03e325a369bf86a55d
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-05-30 09:20:40 +0330

    feat: add sendmodel

---
+++ models/send.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  refPerson: {
+    type: String,
+    require: true,
+  },
+  refRole: {
+    type: String,
+    require: true,
+  },
+  refCollection: {
+    type: String,
+    require: true,
+  },
+  refDocument: {
+    type: String,
+    require: true,
+  },
+  ipAddress: {
+    type: String,
+  },
+  sendDate: {
+    type: Date,
+    require: true,
+  },
+  parentReceive: {
+    type: String,
+  },
+  receivers: [
+    {
+      refPerson: {
+        type: String,
+        require: true,
+      },
+      refRole: {
+        type: String,
+        require: true,
+      },
+      refUrgency: {
+        type: String,
+        require: true,
+      },
+      viewDate: {
+        type: Date,
+      },
+      lastViewedDate: {
+        type: Date,
+      },
+      comment: {
+        type: String,
+      },
+      observed: {
+        type: Boolean,
+        require: true,
+      }
+    }
+  ]
+})
+
+const sendModel = mongoose.model('send', schema);
+
+export default sendModel;
