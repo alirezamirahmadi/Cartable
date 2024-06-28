249f2c6e9a3aedb30b60379ca0645cf05c858049
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-06-28 17:00:23 +0330

    feat: add route protection on server

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { cookies } from "next/headers";
 
 import "./globals.css";
 import "../dist/tailwind/tailwindOutput.css";
--- app/template.tsx
+++ app/template.tsx
@@ -1,7 +1,17 @@
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import NavBar from "@/components/navbar/navBar";
+import { verifyToken } from "@/utils/token";
 
-export default function RootTemplate({children}: Readonly<{ children: React.ReactNode; }>): React.JSX.Element {
+export default function RootTemplate({ children }: Readonly<{ children: React.ReactNode; }>): React.JSX.Element {
+
+  const currentPath = headers().get("x-current-path");
+  const isLoggedin = verifyToken(cookies().get("token")?.value ?? "");
+
+  !isLoggedin && currentPath !== "/login" && redirect("/login");
+  isLoggedin && currentPath === "/login" && redirect("/");
+
   return (
     <>
       <NavBar />
