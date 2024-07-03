c278484dcf29c48520133a8d64e4d58359c2ec2a
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-07-03 19:19:58 +0330

    feat: add loading.tsx file

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+
+import LoadingComponent from "@/components/general/loading/loading";
+
+export default function Loading(): React.JSX.Element {
+  return (
+    <>
+      <div className="mt-20">
+        <LoadingComponent />
+      </div>
+    </>
+  )
+}
