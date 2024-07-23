f34c1406f430dfe083aefba030bfcf7f01facaae
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-07-23 06:59:40 +0330

    feat: add group model

---
+++ models/group.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  groupName: {
+    type: String,
+    require: true,
+  },
+  root: {
+    type: mongoose.Types.ObjectId,
+  },
+  kind: {
+    type: Number, // 1- category 2- user group
+    require: true,
+  }
+})
+
+const groupModel = mongoose.models.group || mongoose.model("group", schema);
+
+export default groupModel;
