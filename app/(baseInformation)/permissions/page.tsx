03c19b3791dc91f50e0d1994f9afdd86a1f15bd2
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-08-19 08:27:35 +0330

    feat: use selectrole in roles component to add new role permission

--- app/(baseInformation)/permissions/page.tsx
+++ app/(baseInformation)/permissions/page.tsx
@@ -22,7 +22,7 @@ export default function Permission(): React.JSX.Element {
   const [rolesPermission, setRolesPermission] = useState<RoleType[]>([]);
   const [groupsPermission, setGroupsPermission] = useState<GroupType[]>([]);
   const [snackProps, setSnackProps] = useState<SnackProps>({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } });
-  
+
   useEffect(() => {
     Promise.all([
       loadGroupsPermission(),
@@ -51,35 +51,55 @@ export default function Permission(): React.JSX.Element {
   }
 
   const handleRolesAction = async (role: RoleType, action: string) => {
-    selectedPermission && role && action === "Delete" &&
-      await fetch(`api/v1/rolePermissions`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "Application/json"
-        },
-        body: JSON.stringify({ roleId: role._id, permissionIds: [selectedPermission?._id] })
+    switch (action) {
+      case "Delete":
+        selectedPermission && role && deleteRolePermission(role._id);
+        break;
+      case "SelectRole":
+        selectedPermission && role && addRolePermission(role._id);
+        break;
+    }
+  }
+
+  const deleteRolePermission = async (roleId: string) => {
+    await fetch(`api/v1/rolePermissions`, {
+      method: "DELETE",
+      body: JSON.stringify({ roleId: roleId, permissionIds: [selectedPermission?._id] })
+    })
+      .then(res => {
+        if (res.status === 200) {
+          loadRolesPermission();
+          setSnackProps({ context: `مجوز مورد نظر گرفته شد`, isOpen: true, severity: "info", onCloseSnack: () => { setSnackProps({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } }) } });
+        }
       })
-        .then(res => {
-          if (res.status === 200) {
-            loadRolesPermission();
-            setSnackProps({ context: `مجوز مورد نظر گرفته شد`, isOpen: true, severity: "success", onCloseSnack: () => { setSnackProps({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } }) } });
-          }
-        })
+  }
+
+  const addRolePermission = async (roleId: string) => {
+    await fetch(`api/v1/rolePermissions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify({ roleId, permissionIds: [selectedPermission?._id] })
+    })
+      .then(res => {
+        if (res.status === 201) {
+          loadRolesPermission();
+          setSnackProps({ context: `مجوز مورد نظر داده شد`, isOpen: true, severity: "success", onCloseSnack: () => { setSnackProps({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } }) } });
+        }
+      })
   }
 
   const handleGroupsAction = async (group: GroupType, action: string) => {
     selectedPermission && group && action === "Delete" &&
       await fetch(`api/v1/groupPermissions`, {
         method: "DELETE",
-        headers: {
-          "Content-Type": "Application/json"
-        },
         body: JSON.stringify({ groupId: group._id, permissionIds: [selectedPermission._id] })
       })
         .then(res => {
           if (res.status === 200) {
             loadGroupsPermission();
-            setSnackProps({ context: `مجوز مورد نظر گرفته شد`, isOpen: true, severity: "success", onCloseSnack: () => { setSnackProps({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } }) } });
+            setSnackProps({ context: `مجوز مورد نظر گرفته شد`, isOpen: true, severity: "info", onCloseSnack: () => { setSnackProps({ context: "", isOpen: false, severity: "success", onCloseSnack: () => { } }) } });
           }
         })
   }
@@ -97,7 +117,7 @@ export default function Permission(): React.JSX.Element {
             <Box sx={{ px: 1, border: "1px solid lightgray", borderRadius: 1.5 }}>
               <Typography variant="body1" sx={{ mt: 1 }}>سمت های دارای مجوز انتخاب شده</Typography>
               <Divider variant="middle" sx={{ my: 1, mx: "auto" }} />
-              <Roles roles={rolesPermission} omit onAction={handleRolesAction} />
+              <Roles roles={rolesPermission} omit selectRole={selectedPermission && selectedPermission?.kind !== 1 ? true : false} onAction={handleRolesAction} />
             </Box>
             <Box sx={{ px: 1, border: "1px solid lightgray", borderRadius: 1.5 }}>
               <Typography variant="body1" sx={{ mt: 1 }}>گروه های دارای مجوز انتخاب شده</Typography>
@@ -107,7 +127,7 @@ export default function Permission(): React.JSX.Element {
           </Box>
         </Box>
       </Box>
-      
+
       {snackProps.isOpen && <Snack {...snackProps} />}
     </>
   )
