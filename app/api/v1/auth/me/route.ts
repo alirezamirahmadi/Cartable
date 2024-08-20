901de7b26f93d59904ef51b937258a8be7f480c6
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-08-20 12:10:32 +0330

    refactor: me route

--- app/api/v1/auth/me/route.ts
+++ app/api/v1/auth/me/route.ts
@@ -19,28 +19,30 @@ const GET = async () => {
     const person = await personModel.aggregate()
       .match({ "account.username": tokenPayload.username })
       .lookup({ from: "roles", localField: "_id", foreignField: "refPerson", as: "roles" })
+      .lookup({ from: "permissions", localField: "roles.permissions", foreignField: "_id", as: "rolepermissions" })
       .unwind("roles")
       .match({ "roles.isActive": true })
       .match({ "roles.isDefault": true })
       .lookup({ from: "groupmembers", localField: "roles._id", foreignField: "refRole", as: "groupmember" })
       .lookup({ from: "groups", localField: "groupmember.refGroup", foreignField: "_id", as: "groups" })
+      .lookup({ from: "permissions", localField: "groups.permissions", foreignField: "_id", as: "grouppermissions" })
       .project({
         "firstName": 1, "lastName": 1, "roles._id": 1, "roles.root": 1, "roles.title": 1, "roles.isDefault": 1,
-        "roles.permissions": 1, "groups.permissions": 1
+        "rolepermissions": 1, "grouppermissions": 1
       })
       .addFields({ "permissions": [] })
       .limit(1);
 
-    const groupsPermission = new Set();
-    if (person[0].groups) {
-      person[0].groups?.map((group: any) => group.permissions.map((permission: any) => groupsPermission.add(permission.toString())))
-      person[0].groups = null;
+    const rolePermissions = new Set();
+    if (person[0].grouppermissions) {
+      person[0].grouppermissions?.map((permission: any) => rolePermissions.add(permission.title))
+      person[0].grouppermissions = null;
     }
-    if (person[0].roles && person[0].roles.permissions) {
-      person[0].roles?.permissions.map((permission: any) => groupsPermission.add(permission.toString()))
-      person[0].roles.permissions = null;
+    if (person[0].rolepermissions) {
+      person[0].rolepermissions.map((permission: any) => rolePermissions.add(permission.title))
+      person[0].rolepermissions = null;
     }
-    person[0].permissions = Array.from(groupsPermission);
+    person[0].permissions = Array.from(rolePermissions);
 
     return person && Response.json(person[0], { status: 200 });
   }
